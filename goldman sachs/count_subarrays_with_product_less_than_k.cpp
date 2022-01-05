c85bcc96bcae364babd9d5764385edaf1e0b1f94
slayking123 <89866619+slayking123@users.noreply.github.com>
2022-01-05 19:22:01 +0530

    Create count_subarrays_with_product_less_than_k.cpp

---
+++ goldman sachs/count_subarrays_with_product_less_than_k.cpp
@@ -0,0 +1,21 @@
+class Solution{
+  public:
+    int countSubArrayProductLessThanK(const vector<int>& a, int n, long long k) {
+    long long p = 1;
+    int ans = 0;
+    for (int start = 0, end = 0; end < n; end++) {
+        p *= a[end];
+ 
+        while (start < end && p >= k)
+            p /= a[start++];
+ 
+        if (p < k) {
+            int len = end - start + 1;
+            ans += len;
+        }
+    }
+ 
+    return ans;
+    }
+};
+© 2022 GitHub, Inc.
