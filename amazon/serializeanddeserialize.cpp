4181112f5720d061ea19fd299fdd09df6245ed59
slayking123 <89866619+slayking123@users.noreply.github.com>
2022-01-11 18:58:04 +0530

    Create serializeanddeserialize.cpp

---
+++ amazon/serializeanddeserialize.cpp
@@ -0,0 +1,34 @@
+void helper(Node *root, vector<int> &v){
+        if(!root){
+            v.push_back(-1);
+            return;
+        }
+        v.push_back(root->data);
+        helper(root->left, v);
+        helper(root->right, v);
+    }
+    vector<int> serialize(Node *root) 
+    {
+        vector<int> v;
+        helper(root, v);
+        
+        return v;
+    }
+    
+    int i=0;
+    Node * deSerialize(vector<int> &A)
+    {
+        if(A.size() == 0){
+            return NULL;
+        }
+        if(A[i] == -1){
+            i++;
+            return NULL;
+        }
+        
+        Node *root = new Node(A[i++]);
+        root->left = deSerialize(A);
+        root->right = deSerialize(A);
+        
+        return root;
+    }
