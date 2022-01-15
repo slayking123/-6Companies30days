1ad16a42a6825bd184eba159d36fa4db476bb464
slayking123 <89866619+slayking123@users.noreply.github.com>
2022-01-15 22:31:08 +0530

    Update connect_nodes.cpp

--- microsoft/connect_nodes.cpp
+++ microsoft/connect_nodes.cpp
@@ -7,23 +7,17 @@ class Solution
        queue<Node *> q;
        q.push(root);
        q.push(NULL);
-       vector<Node *> v;
        while(q.size()>1)
        {
            Node *f=q.front();
-           v.push_back(f);
            q.pop();
            if(!f){
                q.push(NULL);
                continue;
            }
+           f->nextRight=q.front();
            if(f->left) q.push(f->left);
            if(f->right) q.push(f->right);
        }
-       for(int i=0;i<v.size()-1;i++)
-       {
-           if(v[i]) v[i]->nextRight=v[i+1];
-       }
-       v[v.size()-1]->nextRight=NULL;
     }    
 };
