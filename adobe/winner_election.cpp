c2f89d3527f04430ba5596c88519278991ef9dfb
slayking123 <89866619+slayking123@users.noreply.github.com>
2022-01-21 17:14:22 +0530

    Create winner_election.cpp

---
+++ adobe/winner_election.cpp
@@ -0,0 +1,31 @@
+class Solution{
+  public:
+  
+    //Function to return the name of candidate that received maximum votes.
+    vector<string> winner(string arr[],int n)
+    {
+        unordered_map<string, int> hm;
+        for(int i=0; i<n; i++)
+            hm[arr[i]]++;
+        
+        int maxVote=hm[arr[0]];
+        string winner=arr[0];
+        for(int i=1; i<n; i++)
+        {
+          int currVote=hm[arr[i]];
+          string name=arr[i];
+          if(currVote > maxVote)
+          {
+              maxVote= currVote;
+              winner=name;
+          }
+          else if(currVote==maxVote && winner > name)
+                 winner = name;
+        }
+        
+        vector<string> res;
+        res.push_back(winner);
+        res.push_back(to_string(maxVote));
+        return res;
+    }
+};
