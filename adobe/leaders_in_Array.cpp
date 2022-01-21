9d5f7f0e4f6b1eeccf5b36fc11c2d410defe5f72
slayking123 <89866619+slayking123@users.noreply.github.com>
2022-01-21 17:21:13 +0530

    Create leaders_in_Array.cpp

---
+++ adobe/leaders_in_Array.cpp
@@ -0,0 +1,25 @@
+class Solution{
+    //Function to find the leaders in the array.
+    public:
+    vector<int> leaders(int a[], int n){
+      vector<int> vec;
+      int mx = a[n-1];
+      vec.push_back(mx);
+      for(int i=n-2; i>=0; i--){
+          if(a[i]>=mx)
+          {
+              vec.push_back(a[i]);
+          }
+          mx = max(a[i],mx);
+      }
+      int i=0;
+      int j = vec.size()-1;
+      while(i<j)
+      {
+          swap(vec[i],vec[j]);
+          i++;
+          j--;
+      }
+      return vec;
+   }
+};
